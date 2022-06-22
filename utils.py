38927a17b03ae8adab2b65ea25b68e1d871b313a
RobinWangSD <luw015@ucsd.edu>
2022-06-22 15:26:05 -0700

    Modify reconstructed_create_contact_infos.ipynb to handle the case when no grasps is found for an object, i.e. no npy file exist

--- utils.py
+++ utils.py
@@ -13,7 +13,8 @@ def load_label_from_bfs(
     # the keys are the object names in the env_name
     # the values are the labels generated and saved by the file grasp_label_generation_with_brute_force_sampling_antipodal
 
-    labels_for_env = mobile_pnp_dir / f'src/ArmGraspObjectClutterReconstruction-v0_antipodal_grasp_generation/{recon_env_name}_eval_results'
+    labels_for_env = mobile_pnp_dir / f'src/ArmGraspObjectClutterReconstruction-v0_antipodal_grasp_generation/ArmGraspObjectClutterReconstruction-v0_Ditto_grasp_labels_202206151746/{recon_env_name}_eval_results'
+    # mobile_pnp_dir / f'src/ArmGraspObjectClutterReconstruction-v0_antipodal_grasp_generation/{recon_env_name}_eval_results'
 
     list_obj_name = os.listdir(labels_for_env)
 
@@ -22,13 +23,17 @@ def load_label_from_bfs(
     for obj_name in list_obj_name:
 
         path_to_label = labels_for_env / obj_name / 'grasp_pose_and_labels.npy'
+        try:
+            art_info = np.load(
+                path_to_label,
+                allow_pickle=True
+            ).item()
 
-        art_info = np.load(
-            path_to_label,
-            allow_pickle=True
-        ).item()
-
-        obj_name_to_bfs_label[obj_name] = art_info
+            obj_name_to_bfs_label[obj_name] = art_info[obj_name]
+        except FileNotFoundError:
+            print(f'{path_to_label} is not found. The grasps and labels for this object \
+                is treated as not found, i.e. zero grasp is found. Please verify.')
+            obj_name_to_bfs_label[obj_name] = None
 
     return obj_name_to_bfs_label
 
@@ -42,4 +47,4 @@ def get_path_to_recon_mesh(
 
     test_env_name = recon_env_name.replace('Reconstruction', 'Test')
 
-    return str(mobile_pnp_dir / 'mobile_pnp/current_reconstruction' / test_env_name / obj_name / 'model_vhacd.obj')
+    return str(mobile_pnp_dir / 'current_reconstruction' / test_env_name / obj_name / 'model_vhacd.obj')
